e3ec70700e0ba3dc551edf54e213d3235a543631
Abdelrahman <aabdoo2304@gmail.com>
2024-09-15 10:42:46 +0300

    3shan enta msh argal 7aga

---
+++ Scheds/Views/Shared/Components/AllSchedulesViewComponent/ScheduleViewComponent.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Scheds.Models;
+
+
+namespace Scheds.Views.Shared.ViewComponents.AllSchedulesViewComponent
+{
+    [ViewComponent(Name = "AllSchedulesViewComponent")]
+    public class AllSchedulesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<List<List<ReturnedCardItem>>> model)
+        {
+            return View(model);
+        }
+    }
+}
