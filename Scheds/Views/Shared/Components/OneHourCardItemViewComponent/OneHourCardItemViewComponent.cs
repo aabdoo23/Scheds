e56d5d33982b8358cdf809443209f0f504a7f62b
Abdelrahman <aabdoo2304@gmail.com>
2024-09-19 05:11:19 +0300

    Whole new UI

---
+++ Scheds/Views/Shared/Components/OneHourCardItemViewComponent/OneHourCardItemViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scheds.Models;
+
+
+namespace Scheds.Views.Shared.ViewComponents.CardItemViewComponent
+{
+    [ViewComponent(Name = "OneHourCardItemViewComponent")]
+    public class OneHourCardItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ReturnedCardItem model)
+        {
+            return View(model);
+        }
+    }
+}
