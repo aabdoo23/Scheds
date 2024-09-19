53e3331189d1264bd2ea27da1d095aadda9fb122
Abdelrahman <aabdoo2304@gmail.com>
2024-09-19 17:03:23 +0300

    backend bug

--- Scheds/Models/GenerateAndResultViewModel.cs
+++ Scheds/Models/GenerateAndResultViewModel.cs
@@ -3,6 +3,6 @@ namespace Scheds.Models
     public class GenerateAndResultViewModel
     {
         public GenerateRequest FormRequest { get; set; }
-        public List<List<List<ReturnedCardItem>>> ScheduleResult { get; set; }
+        public List<List<ReturnedCardItem>> ScheduleResult { get; set; }
     }
 }
