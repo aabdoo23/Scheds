5395f84249fc3f8e3b43b8f92722e1651edf1988
aabdoo23-linux <aabdoo2304@gmail.com>
2026-02-01 03:29:48 +0200

    Enhance AccountController to include FrontendSettings for improved URL handling in login redirection

--- Scheds/Controllers/AccountController.cs
+++ Scheds/Controllers/AccountController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Scheds.Domain.Configuration;
 
 namespace Scheds.MVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly FrontendSettings _frontend;
+
+        public AccountController(IOptions<FrontendSettings> frontend)
+        {
+            _frontend = frontend.Value;
+        }
+
         [HttpGet("api/account/me")]
         public IActionResult Me()
         {
@@ -17,7 +26,13 @@ namespace Scheds.MVC.Controllers
         [HttpGet]
         public IActionResult Login(string returnUrl = "/")
         {
-            // Challenge Google authentication
+            var frontendBase = _frontend.Url?.TrimEnd('/') ?? "";
+            var allowed = string.IsNullOrEmpty(returnUrl)
+                || returnUrl.StartsWith('/')
+                || (frontendBase.Length > 0 && returnUrl.StartsWith(frontendBase, StringComparison.OrdinalIgnoreCase));
+            if (!allowed)
+                returnUrl = frontendBase + "/";
+
             var properties = new AuthenticationProperties { RedirectUri = returnUrl };
             return Challenge(properties, "Google");
         }
