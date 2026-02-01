056d72406ab428591fffbe0b7a665817dc5d719b
aabdoo23-linux <aabdoo2304@gmail.com>
2026-02-01 02:05:28 +0200

    lol

--- Scheds/Program.cs
+++ Scheds/Program.cs
@@ -69,6 +69,7 @@ namespace Scheds.MVC
             app.UseSession();
 
             app.UseRouting();
+            app.UseCors();
 
             app.UseAuthentication();
             app.UseAuthorization();
